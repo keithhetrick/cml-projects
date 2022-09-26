d363a78cfd7957414d9879b71ee2a015b3f4e24f
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-09-26 00:03:06 -0500

    initial commit

---
+++ cml-timezone-conversion/index.js
@@ -0,0 +1,18 @@
+const moment = require("moment-timezone");
+
+moment.tz.setDefault("America/Chicago");
+
+let targetTimezone;
+
+if (process.argv.length != 3) {
+  console.log("Usage: node <script-file> <timezone>");
+  process.exit(1);
+} else {
+  targetTimezone = process.argv[2];
+}
+
+console.log(
+  `The time at the ${targetTimezone} timezone is ${moment()
+    .tz(targetTimezone)
+    .format()}`
+);
